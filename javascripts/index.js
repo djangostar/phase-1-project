4c78bcb194a1c38c9b72efa7be7f54b058730f67
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-02-10 21:21:14 -0800

    Live coding question added to the program as a reminder

--- javascripts/index.js
+++ javascripts/index.js
@@ -217,3 +217,21 @@ document.addEventListener('DOMContentLoaded', () => {
     resetEvent();
     credits();
 })
+
+const arr = ['Cake', 'caramels', 'muffin', 'lemon', 'drops', 'chocolate.', 'Tootsie', 'roll', 'lemon', 'drops', 'muffin', 'danish', 'lollipop.', 'Jelly', 'halvah', 'croissant', 'sesame', 'snaps', 'bonbon', 'powder.', 'Tart', 'jelly', 'beans', 'apple', 'pie', 'brownie', 'wafer', 'fruitcake', 'halvah', 'sweet', 'roll', 'bonbon.', 'Soufflé', 'brownie', 'sugar', 'plum', 'halvah', 'powder', 'danish.', 'Chupa', 'chups', 'cookie', 'cupcake', 'wafer', 'sweet', 'roll', 'dragée', 'icing.', 'Cheesecake', 'toffee', 'cupcake', 'tiramisu', 'carrot', 'cake', 'jujubes.', 'Powder', 'tiramisu', 'gingerbread', 'pastry', 'chocolate', 'pastry', 'soufflé', 'candy', 'canes', 'carrot', 'cake.', 'Jelly-o', 'biscuit', 'oat', 'cake', 'gummi', 'bears', 'sweet', 'roll', 'cotton', 'candy', 'carrot', 'cake', 'candy', 'canes.']
+
+//iterate over the array
+//grab each element
+//count each letter of each element
+//add it to the total which should amount 475
+
+//forEach or map
+//reduce to accumulate the total number of letters
+
+let newArr = 0
+
+arr.map(word => {
+    newArr += word.length
+})
+
+console.log(newArr)
